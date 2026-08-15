906262f796153c9a15e58153e5d3b7b1aa2cc576
Aydaan <13565@holbertonstudents.com>
2026-08-16 01:58:51 +0400

    Add StudentClass

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -33,3 +33,30 @@ const printTeacher: printTeacherFunction = (
 };
 
 console.log(printTeacher('John', 'Doe'));
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
