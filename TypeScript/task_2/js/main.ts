8c921c0613c6bf6e45708b94627478f5c6b9a632
Aydaan <13565@holbertonstudents.com>
2026-08-16 02:16:51 +0400

    Add string literal types

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -50,13 +50,25 @@ function isDirector(employee: Director | Teacher): employee is Director {
   return employee instanceof Director;
 }
 
-function executeWork(employee: Director | Teacher): void {
+function executeWork(employee: Director | Teacher): string {
   if (isDirector(employee)) {
-    console.log(employee.workDirectorTasks());
-  } else {
-    console.log(employee.workTeacherTasks());
+    return employee.workDirectorTasks();
   }
+
+  return employee.workTeacherTasks();
 }
 
-executeWork(createEmployee(200));
-executeWork(createEmployee(1000));
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  }
+
+  return 'Teaching History';
+}
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
